55362fef81251d1fbac33f078d2be4f5d8478acd
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-10-19 00:22:10 +0700

    memperbesar dan mempertebal ukuran teks

--- app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
@@ -13,8 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_155.ui.theme.QuestBasicComposable_155Theme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +42,7 @@ fun BasicCompos(modifier: Modifier = Modifier){
             .fillMaxSize()
             .padding(top = 45.dp)
     ) {
-        Text("Login")
+        Text("Login", fontSize = 50.sp, fontWeight = FontWeight.Bold)
     }
 }
 
