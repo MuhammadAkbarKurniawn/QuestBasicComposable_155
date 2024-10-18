cc3c8c519bbc2e0109edbaaa621785c5b37d5a02
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-10-19 00:06:05 +0700

    memanggil fungsi BasicCompos pada preview

--- app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
@@ -44,6 +44,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     QuestBasicComposable_155Theme {
-        Greeting("Android")
+        BasicCompos()
     }
 }
