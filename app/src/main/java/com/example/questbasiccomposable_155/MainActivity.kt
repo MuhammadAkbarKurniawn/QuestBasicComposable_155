6797dfd5bfcdfc5afd0d36a6810aaa105f015f99
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-10-19 00:17:50 +0700

    menyusun elemen agar ditengah dan dari atas ke bawah

--- app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_155.ui.theme.QuestBasicComposable_155Theme
@@ -31,6 +33,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicCompos(modifier: Modifier = Modifier){
     Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
 
     ) {
         Text("Login")
