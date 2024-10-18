ad8be3db39d3a8704079a2429278ebdf8a0f8ed5
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-10-19 00:44:17 +0700

    membuat fungsi image untuk menampilkan gambar

--- app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_155/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,6 +52,9 @@ fun BasicCompos(modifier: Modifier = Modifier){
         Spacer(modifier = Modifier.padding(10.dp))
         Text("Ini adalah halaman Login", fontSize = 30.sp, fontStyle = FontStyle.Italic)
         Spacer(modifier = Modifier.padding(10.dp))
+        Image(painter = painterResource(id = R.drawable.logoumy1),
+            contentDescription = "",
+            modifier = Modifier.clip(CircleShape))
     }
 }
 
